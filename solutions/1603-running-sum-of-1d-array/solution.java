bed12d9ce974ab4c565b0920ebeb7d2cb1fa9973
Guneet <guneet.iam@gmail.com>
2024-07-08 16:13:41 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 42.3 MB (99.98%)

--- solutions/1603-running-sum-of-1d-array/solution.java
+++ solutions/1603-running-sum-of-1d-array/solution.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] ans = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            int x=0;
-            for(int y=0;y<=i;y++){
-                x+=nums[y];
-            }
-            ans[i]=x;
-        }   
-        return ans;      
-}
+        
+        for(int i=1;i<nums.length;i++){
+             nums[i] = nums[i-1]+nums[i];
+        }
+        return nums;
+    }
 }
