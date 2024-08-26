eea8ad8e0a513343976a07ac9684393e43888de1
Guneet <guneet.iam@gmail.com>
2024-08-26 16:43:43 +0000

    [LeetCode] Runtime - 2 ms (94.28%), Memory - 55.3 MB (99.93%)

---
+++ solutions/0238-product-of-array-except-self/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] arr = new int[len];
+        arr[0]=1;
+        for(int i=1;i<len;i++){
+            arr[i] = arr[i-1] * nums[i-1];
+        }
+        int rightproduct=1;
+        for(int i = len-1;i>=0;i--){
+            arr[i] = arr[i]*rightproduct;
+            rightproduct = nums[i] *rightproduct;
+        }
+     
+        return arr;
+    }
+}
