d617e11a9bd83d183ef0e2cfacc00da537ff217e
Guneet <guneet.iam@gmail.com>
2024-12-16 09:10:29 +0000

    [LeetCode] Runtime - 49 ms (8.23%), Memory - 45.1 MB (99.91%)

--- solutions/0001-two-sum/solution.java
+++ solutions/0001-two-sum/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       int[] output = new int[2]; 
-        for (int i =0;i<nums.length;i++){
-            int sum = 0 ;
+        int[] sol = {0,0};
+        for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j] == target){
-                    output[0] = i;
-                    output[1] = j;
-                    return output;
+                    sol[0]=i;
+                    sol[1]=j;
                 }
             }
         }
-        return null;
+        return sol;
     }
 }
