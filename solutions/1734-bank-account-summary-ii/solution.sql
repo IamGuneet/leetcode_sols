8ea4adc4cdd95650496231c250bade70547cb45d
Guneet <guneet.iam@gmail.com>
2026-07-08 07:28:58 +0000

    [LeetCode] Runtime - 373 ms (68.48%), Memory - 0.0B (100.00%)

---
+++ solutions/1734-bank-account-summary-ii/solution.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select u.name as NAME , sum(t.amount) as BALANCE
+from Users u 
+join Transactions t
+on u.account = t.account
+group by u.name
+having sum(t.amount) >10000
+
